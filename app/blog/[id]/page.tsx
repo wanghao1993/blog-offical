aa4ac55221a2fc311bcea2361fc200516eb7708e
wanghao1993 <-global>
2024-07-22 18:10:33 +0800

    feat: blog detail

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,86 +1,64 @@
-"use client";
 import MainLayout from "@/components/Layouts/MainLayout";
 import { get } from "@/lib/fetch";
 import { ArticleType } from "@/types/article";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Spin, Empty, Divider, message } from "antd";
+import { Empty, Divider } from "antd";
 import { CalendarOutlined, EyeOutlined, LikeOutlined } from "@ant-design/icons";
 import formatterDate from "@/lib/data_utils";
 import { motion } from "framer-motion";
 import { Viewer } from "@bytemd/react";
 import "juejin-markdown-themes/dist/mk-cute.css";
 import Link from "next/link";
-export default function ArticleDetail() {
-  const { id } = useParams();
-  const [detail, setDetail] = useState<ArticleType.ArticleItem>();
-  const [loading, setLoading] = useState(false);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    if (id) {
-      setLoading(true);
-      get<ArticleType.ArticleItem>("articles/detail", {
-        id,
-      })
-        .then((res) => {
-          setDetail(res);
-        })
-        .finally(() => {
-          setLoaded(true);
-          setLoading(false);
-        });
-    } else {
-      message.warning("id不存在");
-    }
-    return () => {
-      setLoaded(false);
-      setLoading(false);
-    };
-  }, [id]);
-
+async function getBlogDetail(id: string) {
+  const res = await get<ArticleType.ArticleItem>("articles/detail", {
+    id,
+  });
+  return res;
+}
+export default async function ArticleDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
+  getBlogDetail(params.id);
+  const detail: ArticleType.ArticleItem = await getBlogDetail(params.id);
   return (
     <MainLayout>
-      <Spin spinning={loading}>
-        {detail ? (
-          <div className="article-detail">
-            <h1 className="font-semibold mb-4">{detail.title}</h1>
-            <div className="text-sm text-slate-400 flex items-center justify-between  ">
-              <div className="flex items-center ">
-                <div>
-                  <CalendarOutlined />
-                  <span className="pl-1">
-                    {formatterDate(detail.createdAt)}
-                  </span>
-                </div>
-                <Divider type="vertical" className="mx-4!"></Divider>
-                <div>
-                  <EyeOutlined />
-                  <span className="pl-1">{detail.viewsCount}</span>
-                </div>
-                <Divider type="vertical" className="mx-2!"></Divider>
-                <motion.div
-                  className="cursor-pointer"
-                  whileHover={{ scale: 1.2 }}
-                >
-                  <LikeOutlined />
-                  <span className="pl-1">{detail.likesCount}</span>
-                </motion.div>
+      {detail ? (
+        <div className="article-detail">
+          <h1 className="font-semibold mb-4">{detail.title}</h1>
+          <div className="text-sm text-slate-400 flex items-center justify-between  ">
+            <div className="flex items-center ">
+              <div>
+                <CalendarOutlined />
+                <span className="pl-1">{formatterDate(detail.createdAt)}</span>
               </div>
-              <div className="text-primary">
-                <span>{new Date(detail.updatedAt).toLocaleString()}</span>
-                <Divider type="vertical" className="mx-2!"></Divider>
-                <Link href={`/admin/write?id=${detail._id}`}>编辑</Link>
+              <Divider type="vertical" className="mx-4!"></Divider>
+              <div>
+                <EyeOutlined />
+                <span className="pl-1">{detail.viewsCount}</span>
               </div>
+              <Divider type="vertical" className="mx-2!"></Divider>
+              <motion.div
+                className="cursor-pointer"
+                whileHover={{ scale: 1.2 }}
+              >
+                <LikeOutlined />
+                <span className="pl-1">{detail.likesCount}</span>
+              </motion.div>
             </div>
-            <article className="mt-2">
-              <Viewer value={detail.content}></Viewer>
-            </article>
+            <div className="text-primary">
+              <span>{new Date(detail.updatedAt).toLocaleString()}</span>
+              <Divider type="vertical" className="mx-2!"></Divider>
+              <Link href={`/admin/write?id=${detail._id}`}>编辑</Link>
+            </div>
           </div>
-        ) : (
-          loaded && <Empty></Empty>
-        )}
-      </Spin>
+          <article className="mt-2">
+            <Viewer value={detail.content}></Viewer>
+          </article>
+        </div>
+      ) : (
+        <Empty></Empty>
+      )}
     </MainLayout>
   );
 }

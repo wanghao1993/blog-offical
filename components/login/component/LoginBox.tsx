cae6fbe7a82e0d96a57781b4282e27203d3bd1a4
wanghao1993 <whao53333@gmail.com>
2024-08-02 01:49:39 +0800

    fix

--- components/login/component/LoginBox.tsx
+++ components/login/component/LoginBox.tsx
@@ -5,7 +5,7 @@ import { GithubOutlined, GoogleOutlined } from "@ant-design/icons";
 export default function LoginBox() {
   const sign = async (type: "github" | "google") => {
     await signIn(type, {
-      callbackUrl: "https://www.super-super.cn/",
+      callbackUrl: location.origin,
     });
   };
   return (

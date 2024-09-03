842dcdfe3b6d76a7b72e5bd4f84edd7606831838
wanghao1993 <whao53333@gmail.com>
2024-09-04 02:08:26 +0800

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,11 +5,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = "https://super-super.cn/";
   const allBlogs = getAllPostsMeta();
   const blogRoutes = allBlogs.map((post) => ({
-    url: `${siteUrl}/${post.key}`,
+    url: `${siteUrl}/blog/${post.key}`,
     lastModified: post.date,
   }));
 
-  const routes = ["", "blog", "about"].map((route) => ({
+  const routes = ["", "about"].map((route) => ({
     url: `${siteUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));

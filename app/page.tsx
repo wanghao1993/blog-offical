c311c64c9b7ba3f7d0ad17d2dd76496ef56be694
wanghao1993 <whao53333@gmail.com>
2024-10-10 16:02:46 +0800

    feat: 增加监控

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,22 @@ import Works from "@/components/Works";
 import { Suspense } from "react";
 import RecentPosts from "@/components/RecentPost";
 export const dynamic = "force-dynamic";
+// 引入后进行初始化
+import Aegis from "aegis-web-sdk";
+
+new Aegis({
+  id: "nGRv4iKEmEZDVkvnPL", // 上报 id
+  uin: "isaacwang", // 用户唯一 ID（可选）
+  reportApiSpeed: true, // 接口测速
+  reportAssetSpeed: true, // 静态资源测速
+  spa: true, // spa 应用页面跳转的时候开启 pv 计算
+  hostUrl: "https://super-super.cn/",
+});
 export default function Home() {
   return (
     <div>
-      <Intro></Intro>
-      <Works></Works>
+      <Intro />
+      <Works />
       <RecentPosts />
     </div>
   );

0f72891ea71cbef89af079950666e00e5c6ab1ed
wanghao1993 <whao53333@gmail.com>
2024-10-10 16:59:48 +0800

    feat: 增加腾讯性能监控和数据上报

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,18 @@ import Works from "@/components/Works";
 // import Skills from "@/components/Skills";
 // import Projects from "@/components/Projects";
 // import Contact from "@/components/Contact";
-import { Suspense } from "react";
 import RecentPosts from "@/components/RecentPost";
 export const dynamic = "force-dynamic";
-// 引入后进行初始化
-import Aegis from "aegis-web-sdk";
+import dynamicImport from "next/dynamic";
 
-new Aegis({
-  id: "nGRv4iKEmEZDVkvnPL", // 上报 id
-  uin: "isaacwang", // 用户唯一 ID（可选）
-  reportApiSpeed: true, // 接口测速
-  reportAssetSpeed: true, // 静态资源测速
-  spa: true, // spa 应用页面跳转的时候开启 pv 计算
-  hostUrl: "https://super-super.cn/",
+const LoadScript = dynamicImport(() => import("@/components/Script"), {
+  ssr: false,
 });
+
 export default function Home() {
   return (
     <div>
+      <LoadScript />
       <Intro />
       <Works />
       <RecentPosts />
---
+++ components/Script/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+// 引入后进行初始化
+import Aegis from "aegis-web-sdk";
+import { useEffect } from "react";
+export default function LoadScript() {
+  useEffect(() => {
+    if (typeof Aegis === "function") {
+      new Aegis({
+        id: "3oOZnfLdDd2ExXl11g",
+        reportApiSpeed: true,
+        reportAssetSpeed: true,
+        spa: true,
+        hostUrl: "https://rumt-sg.com",
+      });
+    }
+  });
+  return <></>;
+}

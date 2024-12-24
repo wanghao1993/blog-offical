b0daee39d595d5cd2c1e346953069c21d43e7146
wanghao1993 <whao53333@gmail.com>
2024-12-24 15:58:14 +0800

    feat: 增加AI总结

--- components/AiSummary/index.tsx
+++ components/AiSummary/index.tsx
@@ -1,12 +1,15 @@
+"use server";
+
 import model from "./genAi";
 
 async function getSummary(content: string) {
+  "use server";
   const prompt =
     "请帮我总结以下内容，限制在300个字体内，提供中英文版本：" + content;
 
   try {
     const result = await model.generateContent(prompt);
-
+    console.log(result, "result");
     return result;
   } catch (e) {
     console.log(e);
@@ -17,9 +20,11 @@ const AISummay = async ({ content }: { content: string }) => {
   const summary = await getSummary(content);
 
   return (
-    <div className="bg-[#28282c] p-3">
-      <div className="py-2 border-b">AI总结</div>
-      <div>{summary}</div>
+    <div className="px-3 bg-[#28282c] text-white">
+      <div className="py-2 bg-clip-text text-transparent bg-gradient-to-b from-purple-400 to-pink-600">
+        AI总结
+      </div>
+      <div className="py-2">{summary}</div>
     </div>
   );
 };

7df9f154b6543bbbfbd977da3579db0c6ad61b2f
wanghao1993 <whao53333@gmail.com>
2024-12-24 15:35:38 +0800

    feat: 增加AI总结

--- components/AiSummary/genAi.ts
+++ components/AiSummary/genAi.ts
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+console.log(process.env.GOOGLE_API_KEY, "process.env.GOOGLE_API_KEY");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
 
 declare global {
